38b7cf617cf920a0cb4563843e21adc1ed8d78e2
Asrbek <asrbekmahmudov>
2021-11-06 09:59:14 +0500

    Initial Commit

---
+++ mycontactswiftui/mycontactswiftuiApp.swift
@@ -0,0 +1,20 @@
+//
+//  mycontactswiftuiApp.swift
+//  mycontactswiftui
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 06/11/21.
+//
+
+import SwiftUI
+
+@main
+struct mycontactswiftuiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ mycontactswiftuiUITests/mycontactswiftuiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mycontactswiftuiUITestsLaunchTests.swift
+//  mycontactswiftuiUITests
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 06/11/21.
+//
+
+import XCTest
+
+class mycontactswiftuiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
